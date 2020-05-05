6e184d485452aeb7246f8f7b195317a270431b86
Jack187 <anmeldungsadresse@gmx.de>
2020-05-05 21:33:28 +0200

    added support for writting xml file(s) into socket

--- root/defaults/epgupdate.sh
+++ root/defaults/epgupdate.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+writeSocket()
+{
+  FILES=$(find /easyepg/xml -type f -name $XML_FILENAME_PATTERN -printf "%T@ %p\n" | sort -n | cut -d " " -f 2)
+
+  while read -r FILE; do
+    cat ${FILE} | socat - UNIX-CONNECT:/xmltv.sock
+
+    echo "> ${FILE}"
+  done <<< "${FILES}"
+}
+
 chown -R abc:users /easyepg
 chown -R abc:users /tmp
 
@@ -9,5 +20,13 @@ s6-setuidgid abc /bin/bash /easyepg/epg.sh
 
 chown -R abc:users /easyepg
 chown -R abc:users /tmp
+
+if [[ -S /xmltv.sock ]]; then
+  echo "Writing to xmltv.sock..."
+
+  writeSocket
+  sleep 300
+  writeSocket
+fi
 
 exit 0
